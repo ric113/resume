6bbe167ffedb649c075e3c2c857c1153c1afb699
Ricky Hsieh <ric1134991@gmail.com>
2020-01-19 23:33:43 +0800

    initial

---
+++ lang/en.js
@@ -0,0 +1,3 @@
+module.exports = {
+  name: 'Ricky'
+}
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const en = require('./lang/en');
+const tw = require('./lang/zh-tw');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    index: './index.js'
+  },
+  output: {
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'pug-loader',
+            options: {
+              self: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'index.pug',
+      filename: 'index.html',
+      i18n: {
+        ...tw
+      }
+    }),
+    new HtmlWebPackPlugin({
+      template: 'index.pug',
+      filename: 'index.en.html',
+      i18n: {
+        ...en
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+};
